abbc7ccbfe8b4ebeb53a20e5908bc45b648cd3e3
Tanner Mckenney <tmckenney7@outlook.com>
2019-10-14 19:03:58 -0700

    Added Self-contained flag

--- src/View/Cell/MarkdownCell.php
+++ src/View/Cell/MarkdownCell.php
@@ -8,6 +8,10 @@ use Cake\View\Cell;
  */
 class MarkdownCell extends Cell
 {
+    /**
+     * Pandoc Command Constant
+     */
+    const PANDOC_COMMAND = 'pandoc -f markdown -t html';
 
     /**
      * List of valid options that can be passed into this
@@ -28,15 +32,23 @@ class MarkdownCell extends Cell
 
     /**
      * Default display method.
+     * 
+     * @param string The Markdown text to render
+     * @param bool True to trigger the --self-contained flag for pandoc
      *
      * @return void
      */
-    public function display(string $body = null)
+    public function display(string $body = null, bool $selfContained = false)
     {
         if(!empty($body)) {
             $pipes = [];
+            $cmd = self::PANDOC_COMMAND;
 
-            $res = proc_open('pandoc -f markdown -t html', [
+            if($selfContained) {
+                $cmd .= ' --self-contained';
+            }
+
+            $res = proc_open($cmd, [
                 ["pipe", "r"],
                 ["pipe", "w"],
             ], $pipes, TMP);
