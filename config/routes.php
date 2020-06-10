37d1d3214096f45ce3c1f154840c573beadb4f0e
Tanner Mckenney <tmckenney7@outlook.com>
2020-06-09 21:49:32 -0700

    Make Revisions the default landing page.

--- config/routes.php
+++ config/routes.php
@@ -62,7 +62,7 @@ Router::scope('/', function (RouteBuilder $routes) {
      * its action called 'display', and we pass a param to select the view file
      * to use (in this case, src/Template/Pages/home.ctp)...
      */
-    $routes->connect('/', ['controller' => 'Nodes', 'action' => 'index']);
+    $routes->connect('/', ['controller' => 'NodeRevisions', 'action' => 'index']);
 
     /**
      * ...and connect the rest of 'Pages' controller's URLs.
